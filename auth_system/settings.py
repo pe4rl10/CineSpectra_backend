2c4f88bb0d37682a065fc4df2ad23e8ea726438c
Pearl Patel <pearlpatel052@gmail.com>
2024-03-30 15:23:30 +0530

    settings.py updated for deployment

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -28,12 +28,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^%1^y*fkm)nt^ybpzfe5-x@fzvgdk8a2-v*x2=)!($&b6qc-g2'
+# SECRET_KEY = 'django-insecure-^%1^y*fkm)nt^ybpzfe5-x@fzvgdk8a2-v*x2=)!($&b6qc-g2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
@@ -99,8 +101,10 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 #     }
 # }
 
+database_url = os.environ.get('DATABASE_URL')
+
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(database_url)
 }
 
 
